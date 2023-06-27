9bf23718559dbc0f25c94e947b585211bec786ae
nvxden <clever411@yandex.ru>
2023-06-27 22:59:40 +0300

    logging default chats is TgDestination

--- src/utils/tg/tg_logger_stream.py
+++ src/utils/tg/tg_logger_stream.py
@@ -4,15 +4,20 @@ from typing import List
 
 from telebot.async_telebot import AsyncTeleBot
 
+from src.utils.tg.tg_destination import TgDestination
+
 
 class TelegramLoggerStream:
-  def __init__(self, tg: AsyncTeleBot, chats: List[int]):
+  def __init__(self, tg: AsyncTeleBot, chats: List[TgDestination]):
     self.tg = tg
     self.chats = chats
 
   def write(self, report):
     for chat in self.chats:
       asyncio.create_task(
-        self.tg.send_message(chat, report, disable_web_page_preview=True)
+        self.tg.send_message(chat_id=chat.chatId,
+                             reply_to_message_id=chat.messageToReplayId,
+                             text=report,
+                             disable_web_page_preview=True)
       )
     stdout.write(report)
